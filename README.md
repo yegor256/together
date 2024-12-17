882c0c46a940bacdcb2095ea880d0298e43cc8cc
Yegor Bugayenko <yegor256@gmail.com>
2024-12-17 20:36:07 +0300

    #1 start

---
+++ README.md
@@ -0,0 +1,62 @@
+# Runs Java Lambda in Many Threads
+
+[![EO principles respected here](https://www.elegantobjects.org/badge.svg)](https://www.elegantobjects.org)
+[![DevOps By Rultor.com](http://www.rultor.com/b/yegor256/together)](http://www.rultor.com/p/yegor256/together)
+[![We recommend IntelliJ IDEA](https://www.elegantobjects.org/intellij-idea.svg)](https://www.jetbrains.com/idea/)
+
+[![mvn](https://github.com/yegor256/together/actions/workflows/mvn.yml/badge.svg)](https://github.com/yegor256/together/actions/workflows/mvn.yml)
+[![PDD status](http://www.0pdd.com/svg?name=yegor256/together)](http://www.0pdd.com/p?name=yegor256/together)
+[![Maven Central](https://img.shields.io/maven-central/v/com.yegor256/together.svg)](https://maven-badges.herokuapp.com/maven-central/com.yegor256/together)
+[![Javadoc](http://www.javadoc.io/badge/com.yegor256/together.svg)](http://www.javadoc.io/doc/com.yegor256/together)
+[![codecov](https://codecov.io/gh/yegor256/together/branch/master/graph/badge.svg)](https://codecov.io/gh/yegor256/together)
+[![Hits-of-Code](https://hitsofcode.com/github/yegor256/together)](https://hitsofcode.com/view/github/yegor256/together)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/yegor256/together/blob/master/LICENSE.txt)
+
+With this small Java library you can test your objects
+for thread-safety by doing some manipulations with them
+in multiple parallel threads.
+
+First, you add this to your `pom.xml`:
+
+```xml
+<dependency>
+  <groupId>com.yegor256</groupId>
+  <artifactId>together</artifactId>
+  <version>0.0.1</version>
+</dependency>
+```
+
+Then, you use it like this, in your JUnit5 test:
+
+```java
+import com.yegor256.Together;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class FooTest {
+  @Test
+  void worksAsExpected() {
+    MatcherAssert.assertThat(
+      "Compiles without any issues",
+      new Together(),
+      Matchers.allItems(Matchers.equalTo(1))
+    );
+  }
+}
+```
+
+## How to Contribute
+
+Fork repository, make changes, send us a
+[pull request](https://www.yegor256.com/2014/04/15/github-guidelines.html).
+We will review your changes and apply them to the `master` branch shortly,
+provided they don't violate our quality standards. To avoid frustration,
+before sending us your pull request please run full Maven build:
+
+```bash
+mvn clean install -Pqulice
+```
+
+You will need Maven 3.3+ and Java 11+.
---
+++ src/main/java/com/yegor256/Together.java
@@ -0,0 +1,134 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yegor256;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Runs a lambda in multiple threads.
+ *
+ * @param <T> The type of result
+ * @since 0.0.1
+ */
+public final class Together<T> implements Iterable<T> {
+
+    /**
+     * Total number of threads to use.
+     */
+    private final int threads;
+
+    /**
+     * The action to perform.
+     */
+    private final Together.Action<T> action;
+
+    /**
+     * Ctor.
+     * @param act The action
+     */
+    public Together(final Together.Action<T> act) {
+        this(Runtime.getRuntime().availableProcessors(), act);
+    }
+
+    /**
+     * Ctor.
+     * @param total The number of threads
+     * @param act The action
+     */
+    public Together(final int total, final Together.Action<T> act) {
+        this.threads = total;
+        this.action = act;
+    }
+
+    @Override
+    @SuppressWarnings("PMD.CloseResource")
+    public Iterator<T> iterator() {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicBoolean running = new AtomicBoolean();
+        final AtomicInteger overlaps = new AtomicInteger();
+        final ExecutorService service =
+            Executors.newFixedThreadPool(this.threads);
+        try {
+            final Collection<Future<T>> futures =
+                new ArrayList<>(this.threads);
+            for (int pos = 0; pos < this.threads; ++pos) {
+                final int thread = pos;
+                futures.add(
+                    service.submit(
+                        () -> {
+                            latch.await();
+                            if (running.get()) {
+                                overlaps.incrementAndGet();
+                            }
+                            running.set(true);
+                            final T ret = this.action.apply(thread);
+                            running.set(false);
+                            return ret;
+                        }
+                    )
+                );
+            }
+            latch.countDown();
+            final Collection<T> rets = new LinkedList<>();
+            for (final Future<T> future : futures) {
+                try {
+                    rets.add(future.get());
+                } catch (final InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    throw new IllegalArgumentException(ex);
+                } catch (final ExecutionException ex) {
+                    throw new IllegalArgumentException(ex);
+                }
+            }
+            return rets.iterator();
+        } finally {
+            service.shutdown();
+        }
+    }
+
+    /**
+     * Action to perform.
+     *
+     * @param <T> The type of result
+     * @since 0.0.1
+     */
+    public interface Action<T> {
+        /**
+         * Apply it.
+         * @param thread The thread number
+         * @return The result
+         */
+        T apply(int thread);
+    }
+}
